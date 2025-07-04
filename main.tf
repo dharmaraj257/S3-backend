ffc718b3522ce36410bd2620a35c1204bdf925d8
dharmaraj257 <dharmarajkorate257@gmail.com>
2025-07-04 22:49:09 +0530

    commented ec2

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "myawsswarmbucket"
-    key            = "workspace.statefile"
-    region         = "us-east-1"
-    dynamodb_table = "dynamodb-state-locking"
+    bucket = "myawsswarmbucket"
+    key    = "workspace.statefile"
+    region = "us-east-1"
+    # dynamodb_table = "dynamodb-state-locking"
   }
 }
 
